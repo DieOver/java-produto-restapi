30004c0ddfae1973992cb6ce2a910dc43d6dd0cd
Lennon Milicic Costa <lennon.costa@ext.hdi.com.br>
2022-02-02 23:36:27 -0300

    Att

--- src/main/java/com/example/apirest/ApirestApplication.java
+++ src/main/java/com/example/apirest/ApirestApplication.java
@@ -2,7 +2,11 @@ package com.example.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.example.apirest.repository")
+@EntityScan("com.example.apirest.models")
 @SpringBootApplication
 public class ApirestApplication {
 
